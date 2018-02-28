de1c06b708befbe3b4c789c1123566fbc162d1a2
Noumie <lecuona.audrey@gmail.com>
2018-02-28 17:23:08 +0100

    SKIN

--- wp-content/themes/vergerdefred_2017/footer.php
+++ wp-content/themes/vergerdefred_2017/footer.php
@@ -20,6 +20,24 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<div class="menu-mobile" data-turbolinks-permanent>
+	<span class="sprite mobile-fermeture"></span>
+	<a class="menu-mobile-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="logo Le Verger de Fred">
+		<?php include "img/logo_le-verger-de-fred.svg"; ?>
+	</a>
+	<nav>
+		<?php
+			  wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			  ) );
+		?>
+	</nav><!-- #site-navigation -->
+	<a class="phone-menu" href="&#x74;&#x65;&#108;&#x3a;&#43;&#51;&#51;&#x36;&#x30;&#56;&#54;&#x31;&#48;&#57;&#56;&#54;" title="Contactez-nous">
+		<i class="fa fa-phone"></i> &#x20;&#116;&#x65;&#108;&#x3a;&#43;&#x33;&#x33;&#x36;&#56;&#55;&#x38;&#x35;&#x38;&#x36;&#x38;&#x31;
+	</a>
+</div>
+
 <?php wp_footer(); ?>
 
 </body>
